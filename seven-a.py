80f9610231e363341c28ab06160af6cae047ee47
Sourish Sharma <sourish17@yahoo.co.uk>
2023-12-07 05:44:16 +0000

    man i bottled day7

---
+++ seven-a.py
@@ -0,0 +1,62 @@
+from statistics import mode 
+from statistics import median 
+
+t=0 
+x="""32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""".splitlines()
+
+y=[]
+vv="AKQJT98765432"
+vv=vv[::-1]
+v={r:i for i,r in enumerate(vv)}
+print(v)
+
+def d(a):
+    a,b=a.split()
+    b=int(b)
+    h=[]
+    if a==a[0]*5:
+        h.append(10)
+        for i in range(5):
+            h.append(v[a[0]])
+    elif a.count(mode(list(a)))==4:
+        h.append(9)
+        for i in a:h.append(v[i])
+    elif len(set(a))==2:
+        h.append(8)
+        for i in a:h.append(v[i])
+    elif a.count(mode(list(a)))==3:
+        h.append(7)
+        for i in a:h.append(v[i])
+    elif len(set(a))==3 and a.count(mode(list(a)))==2:
+        h.append(6)
+        for i in a:h.append(v[i])
+    elif len(set(a))==4 and a.count(mode(list(a)))==2:
+        h.append(5)
+        for i in a:h.append(v[i])
+    elif len(set(a))==5:
+        h.append(4)
+        for i in a:h.append(v[i])
+    return tuple(h),b
+
+for i in x:
+    a,b=d(i)
+    y.append(a)
+    
+y=sorted(y)
+
+for i,r in enumerate(y):
+    for ii in x:
+        a,b=d(ii)
+        if a==r:
+            t+=(i+1)*b
+            break
+
+
+
+
+print(t) 
+
